baecc5a503aa9608d97fe4044b18b85fcf03b5fd
tony <tony@192.168.31.43>
2018-07-15 13:26:07 +0800

    spring boot+ gradle

---
+++ src/main/java/com/alpha/message/Application.java
@@ -0,0 +1,13 @@
+package com.alpha.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Configuration
+@SpringBootApplication
+public class Application {
+	 public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	    }
+}
--- src/main/java/com/alpha/message/controller/EmailController.java
+++ src/main/java/com/alpha/message/controller/EmailController.java
@@ -1,5 +1,13 @@
 package com.alpha.message.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class EmailController {
 
+	 @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot!";
+	    }
 }
